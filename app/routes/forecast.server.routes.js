b46675ec6866d3fba105f0c3da456259b0c5aa23
Maryamnizam1 <maryamnizam41@gmail.com>
2026-04-03 13:17:54 +0100

    Fix forecast history route

--- app/routes/forecast.server.routes.js
+++ app/routes/forecast.server.routes.js
@@ -1,16 +1,35 @@
+// const forecast = require('../controllers/forecast.server.controllers');
+
+// module.exports = function(app) {
+
+//  app.get('/api/forecast/history/test', (req, res) => {
+//         return res.status(200).send({ message: "history route found" });
+//     });
+
+//     // Get saved forecast history
+//     app.routes('/api/forecast/history')
+//         .get(forecast.getForecastHistory);
+
+//     // Get forecast for all products for a given date
+//     app.routes('/api/forecast')
+//         .get(forecast.getForecastAllProducts)
+//         .post(forecast.saveForecast);
+
+//     // Get forecast for a specific product
+//     app.routes('/api/forecast/:product_id')
+//         .get(forecast.getForecastForProduct);
+    
+// };
+
 const forecast = require('../controllers/forecast.server.controllers');
 
 module.exports = function(app) {
-    // Get forecast for all products for a given date
-    app.route('/api/forecast')
-        .get(forecast.getForecastAllProducts)
-        .post(forecast.saveForecast);
 
-    // Get forecast for a specific product
-    app.route('/api/forecast/:product_id')
-        .get(forecast.getForecastForProduct);
+    app.get('/api/forecast/history', forecast.getForecastHistory);
 
-    // Get saved forecast history
-    app.route('/api/forecast/history')
-        .get(forecast.getForecastHistory);
+    app.get('/api/forecast', forecast.getForecastAllProducts);
+    app.post('/api/forecast', forecast.saveForecast);
+
+    app.get('/api/forecast/:product_id', forecast.getForecastForProduct);
+
 };
